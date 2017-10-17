5ccbdb6fea4c4a7d18912222bc79c25afdfac5db
DDoSolitary <DDoSolitary@gmail.com>
2017-10-17 16:53:06 +0800

    Add the private key.

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 adduser -D builder
 addgroup builder abuild
+
+cat >> /etc/abuild.conf <<- EOF
+	PACKAGER="DDoSolitary <DDoSolitary@gmail.com>"
+	PACKAGER_PRIVKEY="$PWD/privkey"
+EOF
