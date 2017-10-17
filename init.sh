e049d3d00f516bd2e44d2fc7d38e68293b397af0
DDoSolitary <DDoSolitary@gmail.com>
2017-10-17 18:32:52 +0800

    Not sure if particular naming of the public key is required.

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ cat >> /etc/abuild.conf <<- EOF
 	PACKAGER_PRIVKEY="$PWD/privkey"
 EOF
 
-cp pubkey /etc/apk/keys/
+cp pubkey /etc/apk/keys/DDoSolitary@gmail.rsa.pub
 
 for i in $(cat build-list); do
 	chmod 777 "$i"
