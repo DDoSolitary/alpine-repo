DDoSolitary/alpine-repo

commit 56a9bc62e89a8d534aa6d0073bdcd2922e53eb0b

Make use of the "-R" option of abuild.

build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-for i in $(cat build-list); do
+for i in $(find -mindepth 1 -maxdepth 1 -type d ! -path "./.git"); do
 	cd "$i"
-	abuild -r
+	abuild -R
 	cd ..
 done

build-list
@@ -1,4 +1,0 @@
-libbloom
-libcork
-libcorkipset
-shadowsocks-libev

commit 1dea2819196bd2961e066be51ffa6f86fa5a5018

Fix the build script broken since 56a9bc62e89a8d534aa6d0073bdcd2922e53eb0b.

.travis.yml
@@ -5,6 +5,7 @@ install:
   - chmod +x alpine-chroot-install
   - sudo ./alpine-chroot-install -b edge -p "alpine-sdk bash"
   - echo $PRIVKEY | base64 -d > DDoSolitary@gmail.com-00000000.rsa
+  - find -mindepth 1 -maxdepth 1 -type d ! -path "./.git" > build-list
   - /alpine/enter-chroot ./init.sh
 script:
   - /alpine/enter-chroot -u builder ./build.sh

build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in $(find -mindepth 1 -maxdepth 1 -type d ! -path "./.git"); do
+for i in $(cat build-list); do
 	cd "$i"
 	abuild -R
 	cd ..