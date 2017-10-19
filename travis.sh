0e1dacd5dfe9e7fbdf8cfcce3f515ec0f6c4df63
DDoSolitary <DDoSolitary@gmail.com>
2017-10-19 18:58:22 +0800

    Use root to mount sshfs.

--- travis.sh
+++ travis.sh
@@ -16,22 +16,21 @@ find -mindepth 1 -maxdepth 1 -type d ! -path "./.git" > build-list
 echo $PRIVKEY | base64 -d > DDoSolitary@gmail.com-00000000.rsa
 sudo wget -P /alpine/etc/apk/keys https://alpine-repo.sourceforge.io/DDoSolitary@gmail.com-00000000.rsa.pub
 
-# Prepare for connecting to the web server
-echo $DEPLOYKEY | base64 -d > ~/.ssh/id_ed25519
-chmod 600 ~/.ssh/id_ed25519
-cp known_hosts ~/.ssh/
-MOUNT_POINT=/alpine/home/builder/packages/alpine-repo
-
 # Mount the web server's filesystem
-sudo mkdir -p "$MOUNT_POINT"
-sudo bash -c 'echo user_allow_other >> /etc/fuse.conf'
-sudo chmod 644 /etc/fuse.conf
-sshfs -o allow_other \
-	ddosolitary@web.sourceforge.net:/home/project-web/alpine-repo/htdocs/packages \
-	"$MOUNT_POINT"
+MOUNT_POINT=/alpine/home/builder/packages/alpine-repo
+sudo bash -c "
+	echo $DEPLOYKEY | base64 -d > /root/.ssh/id_ed25519
+	chmod 600 /root/.ssh/id_ed25519
+	cp known_hosts /root/.ssh/
+
+	mkdir -p $MOUNT_POINT
+	sshfs -o allow_other \
+		ddosolitary@web.sourceforge.net:/home/project-web/alpine-repo/htdocs/packages \
+		$MOUNT_POINT
+"
 
 # Build the packages
 /alpine/enter-chroot -u builder ./build.sh
 
 # Unmount the web server's filesystem
-fusermount -u "$MOUNT_POINT"
+sudo fusermount -u $MOUNT_POINT
