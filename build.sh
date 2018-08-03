2dd9369710535a92442efb9d834f07c3e83d6a80
DDoSolitary <DDoSolitary@gmail.com>
2018-08-03 18:13:40 +0800

    Migrate to AppVeyor.

--- build.sh
+++ build.sh
@@ -24,11 +24,16 @@ set +x
 echo "$DEPLOYKEY" | base64 -d > /root/.ssh/id_ed25519
 set -x
 chmod 600 /root/.ssh/id_ed25519
-cp .travis/known_hosts /root/.ssh/
+cp known_hosts /root/.ssh/
 mkdir -p "$mount_point"
 sshfs -o allow_other \
 	"ddosolitary@web.sourceforge.net:/home/project-web/alpine-repo/htdocs/packages/$ARCH" \
 	"$mount_point"
+
+# Start SSH server
+if [ "$APPVEYOR_SSH_BLOCK" == "true" ]; then
+	su -m appveyor -c "curl -sflL https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-ssh.sh | bash -e"
+fi
 
 # Build the packages
 /alpine/enter-chroot bash -c "adduser -D builder && addgroup builder abuild"
