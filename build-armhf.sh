1bf742c178f401002705663306314f0533e9a666
DDoSolitary <DDoSolitary@gmail.com>
2019-02-27 13:18:00 +0900

    Download dtb directly.

--- build-armhf.sh
+++ build-armhf.sh
@@ -1,8 +1,5 @@
-curl -o APKBUILD https://git.alpinelinux.org/aports/plain/main/linux-vanilla/APKBUILD?h=3.8-stable
-kver=$(awk -F = '$1 == "pkgver" { print $2 }' APKBUILD)
-krel=$(awk -F = '$1 == "pkgrel" { print $2 }' APKBUILD)
 dtb_name=vexpress-v2p-ca15-tc1.dtb
-curl http://dl-cdn.alpinelinux.org/alpine/v3.8/main/armhf/linux-vanilla-$kver-r$krel.apk | tar xz --warning=none --strip=3 usr/lib/linux-$kver-$krel-vanilla/$dtb_name
+curl -O http://dl-cdn.alpinelinux.org/alpine/v3.8/releases/armhf/netboot/dtbs/$dtb_name
 
 QEMU_SYSTEM=qemu-system-arm
 KERNEL_ARGS="console=ttyAMA0"
