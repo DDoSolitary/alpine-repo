4e8fa67b89355aa7a8d9e1423adbf0eb760f5660
DDoSolitary <DDoSolitary@gmail.com>
2019-03-11 21:30:54 +0800

    [ci skip] Use dtb file from edge repo.

--- build-armhf.sh
+++ build-armhf.sh
@@ -1,5 +1,5 @@
 dtb_name=vexpress-v2p-ca15-tc1.dtb
-curl -O http://dl-cdn.alpinelinux.org/alpine/v3.8/releases/armhf/netboot/dtbs/$dtb_name
+curl -O http://dl-cdn.alpinelinux.org/alpine/edge/releases/armhf/netboot/dtbs/$dtb_name
 
 QEMU_SYSTEM=qemu-system-arm
 KERNEL_ARGS="console=ttyAMA0"
