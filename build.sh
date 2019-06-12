b094b1ad6dae849f936b28edc1e67a7b81f4f657
DDoSolitary <DDoSolitary@gmail.com>
2019-06-12 11:22:44 +0800

    Add sshfs option allow_root.

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ sudo apk upgrade
 keyname="$(basename ~/*.rsa)"
 sudo openssl rsa -pubout -in ~/"$keyname" -out "/etc/apk/keys/$keyname.pub"
 if [ "$ARCH" == "s390x" ]; then sudo chmod 666 /dev/fuse; fi
-sshfs ddosolitary@web.sourceforge.net:/home/project-web/alpine-repo/htdocs/packages/$ARCH ~/packages/alpine-repo/$ARCH
+sshfs -o allow_root \
+	ddosolitary@web.sourceforge.net:/home/project-web/alpine-repo/htdocs/packages/$ARCH \
+	~/packages/alpine-repo/$ARCH
 build_err=0
 for i in $(find -maxdepth 2 -path "*/APKBUILD" -exec dirname "{}" \;); do
 	cd "$i"
