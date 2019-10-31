8bfde9a00a66d2e32a3b190b00ec67dd425d8953
DDoSolitary <DDoSolitary@gmail.com>
2019-11-01 07:30:05 +0800

    Enable caching of s3fs.

--- build.sh
+++ build.sh
@@ -5,9 +5,11 @@ keyname="$(basename ~/*.rsa)"
 sudo openssl rsa -pubout -in ~/"$keyname" -out "/etc/apk/keys/$keyname.pub"
 echo $S3FS_PWD > s3fs_pwd
 chmod 600 s3fs_pwd
+mkdir s3fs_cache
 s3fs alpine-ddosolitary ~/packages/alpine-repo \
 	-o allow_other \
 	-o passwd_file="$PWD/s3fs_pwd" \
+	-o use_cache="$PWD/s3fs_cache"
 	-o url=https://ewr1.vultrobjects.com/ \
 	-o use_path_request_style
 build_err=0
