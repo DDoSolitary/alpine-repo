7af24f980d2608a2db0b8db916dad7f1e8ddba27
DDoSolitary <DDoSolitary@gmail.com>
2019-11-01 00:00:59 +0800

    Switch Vultr's object storage service.

--- build.sh
+++ build.sh
@@ -3,14 +3,18 @@
 sudo apk upgrade
 keyname="$(basename ~/*.rsa)"
 sudo openssl rsa -pubout -in ~/"$keyname" -out "/etc/apk/keys/$keyname.pub"
-sshfs -o allow_root \
-	ddosolitary@web.sourceforge.net:/home/project-web/alpine-repo/htdocs/packages/$ARCH \
-	~/packages/alpine-repo/$ARCH
+echo $S3FS_PWD > s3fs_pwd
+chmod 600 s3fs_pwd
+s3fs alpine-ddosolitary ~/packages/alpine-repo \
+	-o allow_other \
+	-o passwd_file="$PWD/s3fs_pwd" \
+	-o url=https://ewr1.vultrobjects.com/ \
+	-o use_path_request_style
 build_err=0
 for i in $(find -maxdepth 2 -path "*/APKBUILD" -exec dirname "{}" \;); do
 	cd "$i"
 	abuild -Rk && abuild cleanoldpkg || build_err=1
 	cd ..
 done
-fusermount3 -u ~/packages/alpine-repo/$ARCH
+fusermount3 -u ~/packages/alpine-repo
 exit $build_err
--- run-docker.sh
+++ run-docker.sh
@@ -1,20 +1,11 @@
 #!/bin/bash -e
 
-if [ "$CI" == "true" ]; then
-	mkdir -p ~/.ssh
-	echo "$DEPLOYKEY" | base64 -d > ~/.ssh/id_ed25519
-	chmod 600 ~/.ssh/id_ed25519
-	eval $(ssh-agent)
-	ssh-add ~/.ssh/id_ed25519
-fi
-
 docker run -t -p 2200:22 ddosolitary/alpine-builder:$ARCH &
 ssh="ssh -p 2200 \
 	-o UserKnownHostsFile=/dev/null \
 	-o StrictHostKeyChecking=no \
-	-o ForwardAgent=yes \
 	builder@127.0.0.1"
 while [ "$($ssh echo test 2> /dev/null)" != "test" ]; do sleep 1; done
 echo $PRIVKEY | base64 -d | $ssh "cat > DDoSolitary@gmail.com-00000000.rsa"
 tar c build.sh $(echo */APKBUILD | xargs dirname) | $ssh "tar xC alpine-repo"
-$ssh "cd alpine-repo && ARCH=$ARCH ./build.sh"
+$ssh "cd alpine-repo && S3FS_PWD=$S3FS_PWD ARCH=$ARCH ./build.sh"
