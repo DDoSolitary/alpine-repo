6a481e043f3ce1723aa9ff32cfbbf9cb9e007157
DDoSolitary <DDoSolitary@gmail.com>
2020-02-09 18:33:13 +0800

    Specify package build order manually.

--- build.sh
+++ build.sh
@@ -6,11 +6,19 @@ sudo openssl rsa -pubout -in ~/"$keyname" -out "/etc/apk/keys/$keyname.pub"
 sshfs -o allow_root \
 	ddosolitary@web.sourceforge.net:/home/project-web/alpine-repo/htdocs/packages/$ARCH \
 	~/packages/alpine-repo/$ARCH
+
+tmp1="$(mktemp)"
+tmp2="$(mktemp)"
+tsort build-order >> "$tmp1"
+find . -maxdepth 2 -path "*/APKBUILD" | xargs -n1 dirname | xargs -n1 basename >> "$tmp2"
+pkglist="$(cat "$tmp1") $(sort "$tmp1" | comm -3 - "$tmp2")"
+
 build_err=0
-for i in $(find -maxdepth 2 -path "*/APKBUILD" -exec dirname "{}" \;); do
+for i in $pkglist; do
 	cd "$i"
-	abuild -Rk && abuild cleanoldpkg || build_err=1
+	abuild -rk && abuild cleanoldpkg || build_err=1
 	cd ..
 done
+
 fusermount3 -u ~/packages/alpine-repo/$ARCH
 exit $build_err
